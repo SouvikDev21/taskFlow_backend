e825ff8a1f51c075967eab01a7179a7d639d092e
SouvikDev21 <souvik.dey@npstx.com>
2026-08-20 14:40:56 +0530

    Initialised Todo and User Repositories

---
+++ src/main/java/com/example/demo/Repository/TodoRepository.java
@@ -0,0 +1,12 @@
+package com.example.demo.Repository;
+
+import com.example.demo.Entity.Todo;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface TodoRepository extends JpaRepository<Todo,Long> {
+    List<Todo> findByUserId(Long id);
+}
